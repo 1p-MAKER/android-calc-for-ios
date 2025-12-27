49b47c106f05297dcec61e70d814b8059df51802
1p-MAKER <vCWNf87h8RZQacg>
2025-12-28 07:07:28 +0900

    Initial commit with Next.js, Tailwind, and Capacitor

---
+++ android/app/src/main/java/com/nagi/androidcalc/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nagi.androidcalc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nagi.androidcalc',
+  appName: 'AndroidCalc',
+  webDir: 'out'
+};
+
+export default config;
